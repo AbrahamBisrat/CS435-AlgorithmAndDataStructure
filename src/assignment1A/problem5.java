53bde4f35afef279a2fb0f1b0443b9cc2ca690c7
Abraham Bisrat <abrahammehari0@gmail.com>
2022-05-24 08:21:03 -0500

    refactored problem 5

--- src/assignment1A/problem5.java
+++ src/assignment1A/problem5.java
@@ -6,15 +6,12 @@ import java.util.List;
 public class problem5 {
 	
 	public static void main(String[] args) {
-		
 		List<Integer> list = new ArrayList<>();
 		for(int i = 10; i > 0; i--)
 			list.add((int) (Math.random() * 10));
-		
 		int k = 2;
 		System.out.println(naiveSearch(list, k));
-		System.out.println(orderedListSearch(problem4.sort(list), k) + " \t" + k);
-		
+		System.out.println(orderedListSearch(problem4.sort(list), k) + " \t" + k);	
 	}
 
 	// Bisection is the best approach here.
@@ -24,13 +21,10 @@ public class problem5 {
 		if(list.get(halfMark) == k)
 			return true;
 		if(list.size() == 0 || list.size() == 1) return false;
-		System.out.println("list: " + list);
-		
 		return list.get(halfMark) < k 
 				? orderedListSearch(list.subList(halfMark, list.size()), k) :
 					orderedListSearch(list.subList(0, halfMark), k);
 	}
-
 	private static boolean naiveSearch(List<Integer> list, int k) {
 		for(Integer each : list)
 			if(each == k)
