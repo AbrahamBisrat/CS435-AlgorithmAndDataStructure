8a3daf1170c2cb60bce82c9617ce10a4393bac97
Abraham Bisrat <abrahammehari0@gmail.com>
2022-05-24 07:09:07 -0500

    Added tests

--- src/assignment1A/problem3.java
+++ src/assignment1A/problem3.java
@@ -4,15 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class problem3 {
-	
+
 	public static void main(String[] args) {
 		List<Integer> list = new ArrayList<>();
-		for(int i = 1; i <= 11; i++)
+		for (int i = 1; i <= 11; i++)
 			list.add(i);
-		list.forEach(System.out::println);
 		int k = 12;
 
 		System.out.println(sumFound(list, k));
+		
+		int[] testA = {1, 3, 9, 4, 8, 5}; 
+		k = 21;
+		System.out.println(sumFound(arrayToList(testA), k));
+		
+		k = 22;
+		System.out.println(sumFound(arrayToList(testA), k));
+		
+		k = 31;
+		System.out.println(sumFound(arrayToList(testA), k));
+		
+		k = 0;
+		System.out.println(sumFound(arrayToList(testA), k));
+		
+	}
+	
+	public static List<Integer> arrayToList(int[] arr) {
+		List<Integer> list = new ArrayList<>();
+		for(int each : arr) 
+			list.add(each);
+		return list;
 	}
 	
 	/**
@@ -22,15 +42,15 @@ public class problem3 {
 	 * @param sum
 	 */
 	private static boolean sumFound(List list, int k) {
-		// logic
 		List<List<Integer>> subsets = Util.ListOps.subsets(list);
-		subsets.forEach(System.out::println);
-		
-		for(List<Integer> subset : subsets) {
+//		subsets.forEach(System.out::println);
+
+		for (List<Integer> subset : subsets) {
 			int sum = 0;
-			for(int each : subset)
+			for (int each : subset)
 				sum += each;
-			if(sum == k) return true;
+			if (sum == k)
+				return true;
 		}
 		return false;
 	}
