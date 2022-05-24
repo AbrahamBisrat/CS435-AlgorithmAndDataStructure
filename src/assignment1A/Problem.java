dd2555607f5008606fbf8f126b64deb2365a1ff8
Abraham Bisrat <abrahammehari0@gmail.com>
2022-05-24 03:59:21 -0500

    added problems

---
+++ src/assignment1A/Problem.java
@@ -0,0 +1,13 @@
+package assignment1A;
+
+public class Problem {
+	
+	public static void main(String[] args) {
+		problemOne();
+	}
+	
+	private static void problemOne() {
+		
+	}
+
+}
--- src/labOne/Demo.java
+++
@@ -1,5 +0,0 @@
-package labOne;
-
-public interface Demo {
-
-}
