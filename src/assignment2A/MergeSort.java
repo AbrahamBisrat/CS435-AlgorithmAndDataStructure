987db1bded1e847a1e0f945d61fb7297e473bd65
Abraham Bisrat <abrahammehari0@gmail.com>
2022-05-27 04:21:39 -0500

    Added merge two sorted arrays

---
+++ src/assignment2A/MergeSort.java
@@ -0,0 +1,37 @@
+package assignment2A;
+
+public class MergeSort {
+	public static void main(String[] args) {
+		int[] x = {1, 4, 5, 8, 17};
+		int[] y = {2, 4, 8, 11, 13, 21, 23, 25};
+		int[] result = merge(x, y);
+		for(int index : result)
+			System.out.print(index + " , ");
+	}
+	
+	private static int[] merge(int[] a, int[]b) {
+		if(a == null || b == null) throw new IllegalArgumentException("Arrays can't be null");
+		int[] resultSet = new int[a.length + b.length];
+		
+		int aPosition, bPosition, resultSetPosition;
+	    aPosition = bPosition = resultSetPosition = 0;
+
+	    while(aPosition < a.length && bPosition < b.length) {
+	        if (a[aPosition] < b[bPosition]) {
+	            resultSet[resultSetPosition++] = a[aPosition++];
+	        } else {
+	            resultSet[resultSetPosition++] = b[bPosition++];
+	        }
+	    }
+
+	    while (aPosition < a.length) {
+	        resultSet[resultSetPosition++] = a[aPosition++];
+	    }
+
+	    while (bPosition < b.length) {
+	        resultSet[resultSetPosition++] = b[bPosition++];
+	    }
+			
+		return resultSet;
+	}
+}
