0475aaca3d7486c63d13ca355361fac93fd03c8e
Abraham Bisrat <abrahammehari0@gmail.com>
2022-06-01 03:55:41 -0500

    cleanup and explanations

--- src/lab5/ReverseSinglyLinkedList.java
+++ src/lab5/ReverseSinglyLinkedList.java
@@ -1,7 +1,5 @@
 package lab5;
 
-import java.util.List;
-
 public class ReverseSinglyLinkedList {
 	public static void main(String[] args) {
 		SinglyLinkedListImp list = new SinglyLinkedListImp();
@@ -15,6 +13,14 @@ public class ReverseSinglyLinkedList {
 		System.out.println(reverse(list));
 		
 	}
+	/**
+	 * The runtime for the algorithm below is O(N).
+	 * Since we are only doing one complete reversed traversal of the linked list
+	 * we are performing the computations n times for a given linked list of size n.
+	 * 
+	 * @param list
+	 * @return reversedList
+	 */
 	private static SinglyLinkedListImp reverse(SinglyLinkedListImp list) {
 		SinglyLinkedListImp temp = new SinglyLinkedListImp();
 		for(int i = list.size() - 1; i >= 0; i--)
