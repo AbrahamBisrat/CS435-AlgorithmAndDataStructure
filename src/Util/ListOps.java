5470cb5d029e33d66b94818bc40e777bb7b42d04
Abraham Bisrat <abrahammehari0@gmail.com>
2022-06-09 05:37:56 -0500

    cleanup

--- src/Util/ListOps.java
+++ src/Util/ListOps.java
@@ -21,7 +21,9 @@ public class ListOps {
 	private static void benchmark(Consumer<String> consumer) {
 		long before = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
 		long startTime = System.currentTimeMillis();
+		
 		consumer.accept(null);
+		
 		long endTime = System.currentTimeMillis();
 		long after = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
         p("Execution time : " + (endTime - startTime) + "ms");
